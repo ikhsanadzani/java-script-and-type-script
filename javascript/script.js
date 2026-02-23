6190270e75c0c9aa6b9b0e6305e5e3cdb27d7f1f
Mas Mochammad Ikhsan Adzani <ikhsanadzni@gmail.com>
2026-02-24 01:02:10 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -8,18 +8,15 @@ const hitungPromo = () => {
     for (let hari = 1; hari <= 7; hari++) {
         let hargaSetelahDiskon = hargaJual * (1 - diskonPenjualan / 100);
 
-        let tarifPajak; {
-            if (hari % 2 === 0) {
-                tarifPajak = 0.20;
-            } else {
-                tarifPajak = 0.125;
-            }
-        }
+        let tarifPajak = (hari % 2 === 0) ? 0.20 : 0.125;
         
         let potonganPajak = hargaSetelahDiskon * tarifPajak;
 
         let pendapatanBersih = hargaSetelahDiskon - potonganPajak;
         let labaRugi = pendapatanBersih - modal;
+
+    console.log ('Hari ${hari}: Rp.${Math.round(labaRugi)}');
+    diskonPenjualan/=2;
     }
 };
 
