fec11d0540acb5a0accc4da24eabae0885e08af6
Mas Mochammad Ikhsan Adzani <ikhsanadzni@gmail.com>
2026-02-24 20:04:13 +0700

    update

--- typescript/script.ts
+++ typescript/script.ts
@@ -2,4 +2,8 @@ const hitungPromo=(): void=> {
     const modal: number = 27000;
     const hargaJual: number = 60000;
     let diskonPenjualan: number = 10;
+
+    for(let hari; hari <= 7; hari++) {
+        
+    }
 }
