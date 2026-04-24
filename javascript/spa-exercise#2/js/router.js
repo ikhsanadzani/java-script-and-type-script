08a010c5fba25ede1db86feac5a85bde44d849e6
ikhsanadzani <ikhsanadzni@gmail.com>
2026-04-25 01:47:50 +0700

    refactor Router class to streamline route handling and improve goToRoute method

--- javascript/spa-exercise#2/js/router.js
+++ javascript/spa-exercise#2/js/router.js
@@ -33,8 +33,30 @@ Router.prototype = {
             for (var i = 0, length = r.length; i < length; i++) {
                 var route = r[i];
                 if (route.isActiveRoute(window.location.hash.substr(1))) {
-                    scope.rootElem.innerHTML = route.htmlName;
+                    scope.goToRoute(route.htmlName);
+                }
+            }
+        } else {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if(route.default) {
+                    scope.goToRoute(route.htmlName);
                 }
         }
     }
-}
+},
+    goToRoute: function (htmlName) {
+        (function(scope){
+            var url = 'views/' + htmlName,
+                xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState === 4 && this.status === 200) {
+                        scope.rootElem.innerHTML = this.responseText;
+                    }
+                };
+                xhttp.open('GET', url, true);
+                xhttp.send();
+        })(this);  
+    }
+
+};
